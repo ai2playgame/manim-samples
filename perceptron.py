6b2f65689b1ddd98c5febc8a8a3d4b9874a84352
ai2playgame <detective9684@gmail.com>
2022-09-04 08:11:56 +0900

    Revert "CRLF -> LF"
    This reverts commit e704d154a5617c394a90c8b810a41a6cc540e6c6.

--- perceptron.py
+++ perceptron.py
@@ -4,8 +4,7 @@ from numpy import *
 
 class Perceptron(Scene):
     def construct(self):
-        ax = Axes(x_range=[-3.5, 3.5], y_range=[-3.5, 3.5],
-                  x_length=7).add_coordinates()
+        ax = Axes(x_range=[-3.5, 3.5], y_range=[-3.5, 3.5], x_length=7).add_coordinates()
         dot1 = Dot(ax.c2p(0, 0), color=RED_A)
         dot2 = Dot(ax.c2p(0, 1), color=BLUE_A)
         dot3 = Dot(ax.c2p(1, 0), color=BLUE_A)
